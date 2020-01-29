5da24c1a32f9652f1de7c478cb7e2e12215e5d70
ADITYA0012345 <43110831+ADITYA0012345@users.noreply.github.com>
2020-01-29 19:46:25 +0530

    Add files via upload
    This following code(s) is for sending multiple sensor data by Radiofrequency(433MHz) module using Arduino. This code sends and receives only the raw sensor's data. It has used sensors DHT11 and Pulse.

---
+++ transmitter.ino
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <RH_ASK.h> 
+#include <SPI.h> 
+int dht, pulse;
+RH_ASK rf_driver;
+
+void setup()
+{
+   Serial.begin(9600);
+   rf_driver.init();   
+}
+
+void loop()
+{
+    char msg[10];
+    dht = analogRead(A0);
+    sprintf(msg, "%d-dht", dht);
+    rf_driver.send((uint8_t*)msg, strlen(msg));
+    rf_driver.waitPacketSent();
+    delay(10);
+    pulse = analogRead(A1);
+    Serial.println(pulse);
+    sprintf(msg, "%d-pul", pulse);
+    rf_driver.send((uint8_t*)msg, strlen(msg));
+    rf_driver.waitPacketSent();
+    delay(1000);
+}
+
